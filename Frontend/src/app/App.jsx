c4c6ee221f71e9b09ff2114b058c4f3f571d81c2
Nayan <nayanbhusari02@gmail.com>
2026-03-18 08:27:01 +0530

    ui layer done

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,5 +1,11 @@
+import { RouterProvider } from "react-router";
+import { router } from "./app.routes.jsx";
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,8 +1,22 @@
 import { createBrowserRouter } from "react-router";
+import Register from "../features/auth/pages/Register";
+import Login from "../features/auth/pages/Login";
 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <h1 className="font-bold text-2xl text-center">
+        Welcome to Perplexity AI 🤖
+      </h1>
+    ),
+  },
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
