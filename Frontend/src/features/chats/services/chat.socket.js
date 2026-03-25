8fa31100e81b0877eeb3396a9af750289082fd82
Nayan <nayanbhusari02@gmail.com>
2026-03-25 16:21:50 +0530

    frotnend socket.io client update for production

--- Frontend/src/features/chats/services/chat.socket.js
+++ Frontend/src/features/chats/services/chat.socket.js
@@ -1,11 +1,18 @@
 import { io } from "socket.io-client";
 
 export const initializeSocketConnection = () => {
-  const socket = io("http://localhost:3000", {
+  const socket = io("https://neurovia-ai-evzk.onrender.com", {
+    transports: ["websocket"], 
     withCredentials: true,
   });
 
   socket.on("connect", () => {
     console.log("connected to socket.io server");
   });
+
+  socket.on("connect_error", (err) => {
+    console.log("Socket error:", err.message);
+  });
+
+  return socket;
 };
